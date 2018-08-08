1df78f6c60f5e3c0296f9ccf80dac1010bfda2b2
sakaok <sakaok@pepabo.com>
2018-08-08 18:45:19 +0900

    ""

---
+++ 3/item_25.py
@@ -0,0 +1,59 @@
+# 親クラスをsuperを使って初期化する
+
+class MyBaseClass(object):
+    def __init__(self, value):
+        self.value = value
+
+class MyChildClass(MyBaseClass):
+    def __init__(self):
+        """親の__init__を直接呼び出す"""
+        MyBaseClass.__init__(self, 5)
+
+# 上の問題は、__init__の呼び出し順序が全てのサブクラス間で
+# 規定されていいないこと。
+class TimesTwo(object):
+    def __init__(self):
+        (self.value *= 2
+
+class PlusFive(object):
+    def __init__(self):
+        self.value += 5
+
+class OneWay(MyBaseClass, TimesTwo, PlusFive):
+    def __init__(self, value):
+        MyBaseClass.__init__(self, value)
+        TimesTwo.__init__(self)
+        PlusFive.__init__(self)
+
+foo = OneWay(5)
+print('First ordering is (5 * 2) + 5 =', foo.value)
+
+
+class AnotherWay(MyBaseClass, PlusFive, TimesTwo):
+    def __init__(self, value):
+        MyBaseClass.__init__(self, value)
+        TimesTwo.__init__(self)
+        PlusFive.__init__(self)
+
+bar = AnotherWay(5)
+print('Second ordering still is', bar.value)
+
+class TimesFive(MyBaseClass):
+    def __init__(self, value):
+        MyBaseClass.__init__(self, value)
+        self.value *= 5
+
+class PlusTwo(MyBaseClass):
+    def __init__(self, value):
+        MyBaseClass.__init__(self, value)
+        self.value += 2
+
+class ThisWay(TimesFive, PlusTwo):
+    def __init__(self, value):
+        TimesFive.__init__(self, value)
+        PlusTwo.__init__(self, value)
+
+foo = ThisWay(5)
+print('Should be (5 * 5) + 2 = 27 but is', foo.value)
+
+
